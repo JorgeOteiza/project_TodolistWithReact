687da6d0877be5141b0cdd44274bca7757d126ad
Jorge Oteiza <oteiza.jor@gmail.com>
2024-03-16 05:49:55 +0000

    lógica lista, falta ajustar detalles de estructura

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/style.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todoListData, setTodoListData] = useState([
+    "Hacer la cama",
+    "Tomar desayuno",
+  ]);
+  const [inputValue, setInputValue] = useState("");
+
+  const printList = () => {
+    return todoListData.length > 0 ? (
+      todoListData.map((todoText, index) => (
+        <article key={index}>
+          <p>
+            {todoText}{" "}
+            <button onClick={() => deleteTodoItem(todoText)}>x</button>
+          </p>
+        </article>
+      ))
+    ) : (
+      <p>La lista está vacía</p>
+    );
+  };
+
+  const addTodoItem = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) return;
+    setTodoListData([...todoListData, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteTodoItem = (todoItemToDelete) => {
+    setTodoListData(todoListData.filter((value) => value !== todoItemToDelete));
+  };
+
+  return (
+    <div>
+      <h1>TODO LIST</h1>
+      <form onSubmit={addTodoItem}>
+        <input
+          name="todoInput"
+          id="todoInput"
+          placeholder="Ej."
+          autoFocus
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </form>
+      <section id="todoList">{printList()}</section>
+    </div>
+  );
 };
 
 export default Home;
