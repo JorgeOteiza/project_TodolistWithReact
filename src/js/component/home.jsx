2fc5d4cacba7a1c0bbe75425ee20817382a60570
Jorge Oteiza <oteiza.jor@gmail.com>
2024-03-16 22:57:31 +0000

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,42 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "../../styles/style.css";
 
 const Home = () => {
-  const [todoListData, setTodoListData] = useState([
-    "Hacer la cama",
-    "Tomar desayuno",
-  ]);
+  const [todoListData, setTodoListData] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
   const printList = () => {
     return todoListData.length > 0 ? (
       todoListData.map((todoText, index) => (
         <article key={index}>
-          <p>
-            {todoText}{" "}
-            <button onClick={() => deleteTodoItem(todoText)}>x</button>
+          <p
+            onMouseEnter={(e) =>
+              (e.target.querySelector("button").style.display = "inline-block")
+            }
+            onMouseLeave={(e) =>
+              (e.target.querySelector("button").style.display = "none")
+            }
+          >
+            <span>{todoText}</span>{" "}
+            <button onClick={() => deleteTodoItem(todoText)}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
           </p>
         </article>
       ))
     ) : (
-      <p>La lista está vacía</p>
+      <p
+        style={{
+          color: "red",
+          fontStyle: "italic",
+          fontSize: "18px",
+          textAlign: "center",
+        }}
+      >
+        La lista está vacía
+      </p>
     );
   };
 
@@ -36,12 +53,12 @@ const Home = () => {
 
   return (
     <div>
-      <h1>TODO LIST</h1>
+      <h1>LISTA</h1>
       <form onSubmit={addTodoItem}>
         <input
           name="todoInput"
           id="todoInput"
-          placeholder="Ej."
+          placeholder="Agregar nuevo"
           autoFocus
           type="text"
           value={inputValue}
